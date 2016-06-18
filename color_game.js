d04f90b7b4b714f0c1ce70c77675959563b62e04
coded5282 <edwardjchen@gmail.com>
2016-06-18 16:37:12 -0400

    functions to change colors

--- color_game.js
+++ color_game.js
@@ -8,8 +8,9 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square"); 
-var pickedColor = colors[3]; 
+var pickedColor = pickColor();  
 var colorDisplay = document.getElementById("colorDisplay"); 
+var messageDisplay = document.querySelector("#message"); 
 
 colorDisplay.textContent = pickedColor; 
 
@@ -25,11 +26,27 @@ for (var i = 0; i < squares.length; i++)
 		// compare color to pickedColor
 		if (clickedColor === pickedColor)
 		{
-			alert("correct"); 
+			messageDisplay.textContent = "Correct!"; 
+			changeColors(clickedColor); 
 		}
 		else
 		{
-			alert("wrong"); 
+			this.style.background = "#232323"; 
+			messageDisplay.textContent = "Try Again"; 
 		}
 	});
+}
+
+function changeColors(color) {
+	// loop through all squares
+	for (var i = 0; i < squares.length; i++)
+	{
+		// change each color to match given color 
+		squares[i].style.background = color; 
+	} 
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random]; 
 }
